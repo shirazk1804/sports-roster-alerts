0bd3d91abe669215d4b766982bd7bef26a3d3f21
Shiraz Khan <shirazk20@gmail.com>
2026-09-24 12:47:30 -0700

    Allow Netlify web app origin

--- backend/src/main/java/sportsalerts/WebConfig.java
+++ backend/src/main/java/sportsalerts/WebConfig.java
@@ -5,23 +5,24 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig
-        implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(
-            CorsRegistry registry) {
-
-        registry
-                .addMapping("/api/**")
-                .allowedOriginPatterns(
-                        "http://localhost:*")
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:8081",
+                        "https://gentle-meerkat-daba19.netlify.app"
+                )
                 .allowedMethods(
                         "GET",
                         "POST",
                         "PUT",
+                        "PATCH",
                         "DELETE",
-                        "OPTIONS")
-                .allowedHeaders("*");
+                        "OPTIONS"
+                )
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
